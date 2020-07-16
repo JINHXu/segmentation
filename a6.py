bb1de7e76856fb59e996175eda87788e24ab1c68
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-07-16 22:04:41 +0200

    branch, update template

--- a6.py
+++ a6.py
@@ -16,11 +16,39 @@ def read_data(filename, eos='#'):
     Returns: (a tuple)
     -----------
     utterances: A list of strings without white spaces 
-    labels:     List of sequences  of 0/1 labels. '1' indicates the
-                corresponding character in 'utterances' begins a word,
-                '0' indicates that it is inside a word.
+    labels:     List of sequences  of 0/1 labels. '1' indicates that the
+                corresponding character in 'utterances' begins a word (B),
+                '0' indicates that it is inside a word (I).
     """
     ### Exercise 6.1
+
+def labels_to_segments(utterances, labels):
+    """Return segmented utterances corresponding (B/I) labels,
+
+    Given a sequence of utterances like
+        ["nightnight#", "daddy#", "akitty#"]
+    and corresponding lables like,
+        [[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], 
+         [1, 0, 0, 0, 0, 1], 
+         [1, 1, 0, 0, 0, 0, 1]]
+    return the segmented utterances similar to:
+        [["night", "night", "#"], ["daddy", "#"], ["a", "kitty", "#"]]
+
+    Note that to use it with model predictions, you should assume a
+    number larger than 0.5 in the 'labels' array indicates a boundary.
+
+    Parameters:
+    -----------
+    utterances:  The list of unsegmented utterances.
+    labels:      The B/O labels (probabilities) for given utterances.
+
+    Returns: 
+    segments:  Segmented utterances.
+
+    """
+    ### You will need this function to avoid duplicating code for
+    ### the main part that re-segments the test utterances,
+    ### and in the segment() function below.
 
 def segment(u_train, l_train, u_test):
     """ Train an RNN sequence labeller on the training set, return segmented test set.
@@ -30,8 +58,8 @@ def segment(u_train, l_train, u_test):
     l_train:  Training set labels, corresponding to each character in 'u_train'
     u_test:   Unsegmented test input
 
-    the format of the u_train and u_test is similar to 'utterances'
-    returned by 'read_datat()'
+    The format of the u_train and u_test is similar to 'utterances'
+    returned by 'read_data()'.
 
 
     Returns: 
@@ -63,6 +91,6 @@ if __name__ == '__main__':
     u_train, l_train = u[:train_size], l[:train_size]
     u_test, l_test = u[train_size:], l[train_size:]
 
-    seg_test = segment_unsupervised(u_train, l_train, u_test)
+    seg_test = segment(u_train, l_train, u_test)
 
-    evaluate(l_test, seg_test)
+    evaluate(labels_to_segments(u_test, l_test), seg_test)
