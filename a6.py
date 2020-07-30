0394d57b49b9f8ee6a38613063bb48950a394849
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-07-31 00:35:55 +0200

    狗屁 6.2

--- a6.py
+++ a6.py
@@ -4,6 +4,13 @@
 
     Jinghua Xu
 """
+import numpy as np
+from sklearn import preprocessing
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+
 
 
 def read_data(filename, eos='#'):
@@ -46,6 +53,138 @@ def read_data(filename, eos='#'):
             labels.append(label)
 
     return utterances, labels
+
+
+class WordEncoder:
+    """An encoder for a sequence of words.
+    The encoder encodes each word as a sequence of one-hot characters.
+    The words that are longer than 'maxlen' is truncated, and
+    the words that are shorter are padded with 0-vectors.
+    Two special symbols, <s> and </s> (beginning of sequence and
+    end of sequence) should be prepended and appended to every word
+    before padding or truncation. You should also reserve a symbol for
+    unknown characters (distinct from the padding).
+    The result is either a 2D vector, where all character vectors
+    (including padding) of a word are concatenated as a single vector,
+    o a 3D vector with a separate vector for each character (see
+    the description of 'transform' below and the assignment sheet
+    for more detail.
+    Parameters:
+    -----------
+    maxlen:  The length that each word (including <s> and </s>) is padded
+             or truncated. If not specified, the fit() method should
+             set it to cover the longest word in the training set. 
+    """
+
+    def __init__(self, maxlen=None):
+        # to be set up in fit()
+        self._maxlen = maxlen
+        self._char2idx = dict()
+        self._nchars = len(self._char2idx)
+
+    def fit(self, words):
+        """Fit the encoder using words.
+        All collection of information/statistics from the training set
+        should be done here.
+        Parameters:
+        -----------
+        words:  The input words used for training.
+        Returns: None
+        """
+        setUPmaxlen = False
+        if self._maxlen is None:
+            self._maxlen = 0
+            setUPmaxlen = True
+
+        # special symbols
+        self._char2idx['<s>'] = 0
+        self._char2idx['</s>'] = 1
+        # reserve for unknown chararacters
+        self._char2idx['uk'] = 2
+
+        # current index
+        idx = 3
+        # chars in words
+        for word in words:
+            if len(word) > self._maxlen and setUPmaxlen:
+                self._maxlen = len(word)
+            for char in word:
+                if char not in self._char2idx:
+                    self._char2idx[char] = idx
+                    idx += 1
+        self._nchars = len(self._char2idx)
+
+    def transform(self, words, pad='right', flat=True):
+        """ Transform a sequence of words to a sequence of one-hot vectors.
+        Transform each character in each word to its one-hot representation,
+        combine them into a larger sequence, and return.
+        The returned sequences formatted as a numpy array with shape 
+        (n_words, max_wordlen * n_chars) if argument 'flat' is true,
+        (n_words, max_wordlen, n_chars) otherwise. In both cases
+        n_words is the number of words, max_wordlen is the maximum
+        word length either set in the constructor, or determined in
+        fit(), and n_chars is the number of unique characters.
+        Parameters:
+        -----------
+        words:  The input words to encode
+        pad:    Padding direction, either 'right' or 'left'
+        flat:   Whether to return a 3D array or a 2D array (see above
+                for explanation)
+        Returns: (a tuple)
+        -----------
+        encoded_data:  encoding the input words (a 2D or 3D numpy array)
+        """
+
+        # params check
+        if isinstance(flat, bool) and (pad == 'right' or pad == 'left'):
+            pass
+        else:
+            raise ValueError(
+                "Illegal Argument! pad can only be 'right' or 'left', flat has to be bool!")
+        encoded_words = []
+        for word in words:
+            word = list(word)
+            encoded_word = []
+            # prepend special char
+            word.insert(0, '<s>')
+            # append special char
+            word.append('</s>')
+            if len(word) > self._maxlen:
+                # truncation
+                word = word[:self._maxlen]
+            for char in word:
+                char_vec = [0]*self._nchars
+                if char in self._char2idx:
+                    idx = self._char2idx[char]
+                    char_vec[idx] = 1
+                else:
+                    # unknown char
+                    char = 'uk'
+                    idx = self._char2idx[char]
+                    char_vec[idx] = 1
+                if flat:
+                    encoded_word = encoded_word + char_vec
+                else:
+                    encoded_word.append(char_vec)
+            if len(word) < self._maxlen:
+                # padding
+                padding = [0]*self._nchars
+                if pad == 'right':
+                    for _ in range(self._maxlen-len(word)):
+                        if flat:
+                            encoded_word = encoded_word + padding
+                        else:
+                            encoded_word.append(padding)
+                else:
+                    for _ in range(self._maxlen-len(word)):
+                        if flat:
+                            encoded_word = padding + encoded_word
+                        else:
+                            encoded_word.insert(0, padding)
+            encoded_words.append(encoded_word)
+        return np.array(encoded_words)
+
+
 
 
 def labels_to_segments(utterances, labels):
@@ -117,13 +256,60 @@ def segment(u_train, l_train, u_test):
     pred_seg:  Predicted segments, a list of list of strings, each
                corresponding to a predicted word.
     """
-    # encode data
+    # encode words
+    we = WordEncoder()
+    we.fit(u_train)
+    encoded_u_train = we.transform(u_train, flat=False)
+    '''
+    print(encoded_u_train)
+    print(encoded_u_train.shape)
+    print(we._char2idx)
+
+    print(l_train)
+    print(type(l_train))
+    '''
+
+
+    # pad labels
+    for l in l_train:
+        if len(l) < encoded_u_train.shape[1]:
+            l.extend([2]*(encoded_u_train.shape[1]-len(l)))
+    
+    l_train = np.array(l_train)
+
+    '''
+    print(len(l_train[0]))
+    print(len(l_train[1]))
+    print(len(l_train[2]))
+    '''
+
+    output_dim = l_train.shape[1]
 
     # train
+    grnn = Sequential()
+    grnn.add(LSTM(64, input_shape=(
+        encoded_u_train.shape[1], encoded_u_train.shape[2]), activation='relu'))
+    grnn.add(Dense(output_dim, activation='softmax'))
+
+    grnn.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+    
+    grnn.fit(encoded_u_train, l_train)
 
     # predict
 
+    # encode test utterance
+    encoded_u_test = we.transform(u_test, flat = False)
+
+    l_test_pred = grnn.predict(encoded_u_test)
+    print(l_test_pred)
+
+    # get rid of the tailling padded numbers?
+
     # labels to segments
+    pred_seg = labels_to_segments(u_test, l_test_pred)
+    print(pred_seg)
 
 
 def evaluate(gold_seg, pred_seg):
@@ -143,19 +329,33 @@ def evaluate(gold_seg, pred_seg):
 
 if __name__ == '__main__':
 
+    
+
     # test read_data
     u, l = read_data('/Users/xujinghua/a6-lahmy98-jinhxu/readdata_test.txt'  # , eos=''
                      )
     print(u)
     print(l)
 
+    segment(u, l, u)
+
+    '''
+
     # test labels_to_segments
     print(labels_to_segments(u, l))
+    
 
 
-'''
+
+    
     # Approximate usage of the exercises (not tested).
     u, l = read_data('br-phono.txt')
+
+
+
+
+
+    
 
     # train-test split
     train_size = int(0.8 * len(u))
